e033517bead37a6ab1d7dd576a85fb6064ff02c0
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-03-03 11:07:15 +0100

    add functional test

--- server.py
+++ server.py
@@ -5,10 +5,10 @@ from datetime import datetime
 from flask import Flask, render_template, request, redirect, flash, url_for, session
 
 
-def load_clubs(path='clubs.json'):
+def load_clubs(path="clubs.json"):
     try:
         with open(path) as c:
-            list_of_clubs = json.load(c).get('clubs', [])
+            list_of_clubs = json.load(c).get("clubs", [])
             return list_of_clubs
     except FileNotFoundError:
         raise FileNotFoundError(f"{path} not found for clubs")
@@ -17,11 +17,10 @@ def load_clubs(path='clubs.json'):
         raise
 
 
-
-def load_competitions(path='competitions.json'):
+def load_competitions(path="competitions.json"):
     try:
         with open(path) as comps:
-            list_of_competitions = json.load(comps).get('competitions', [])
+            list_of_competitions = json.load(comps).get("competitions", [])
             return list_of_competitions
     except FileNotFoundError:
         raise FileNotFoundError(f"{path} not found for competitions")
@@ -30,28 +29,28 @@ def load_competitions(path='competitions.json'):
         raise
 
 
-def update_clubs(path='clubs.json'):
-    with open(path, 'w') as c:
+def update_clubs(path="clubs.json"):
+    with open(path, "w") as c:
         final = {"clubs": clubs}
         json.dump(final, c, indent=4)
 
 
-def update_competitions(path='competitions.json'):
-    with open(path, 'w') as c:
+def update_competitions(path="competitions.json"):
+    with open(path, "w") as c:
         final = {"competitions": competitions}
         json.dump(final, c, indent=4)
 
 
 def find_competition_by_name(name):
-    return next((c for c in competitions if c['name'] == name), None)
+    return next((c for c in competitions if c["name"] == name), None)
 
 
 def find_club_by_name(name):
-    return next((c for c in clubs if c['name'] == name), None)
+    return next((c for c in clubs if c["name"] == name), None)
 
 
 def find_club_by_email(email):
-    return next((c for c in clubs if c['email'] == email), None)
+    return next((c for c in clubs if c["email"] == email), None)
 
 
 def validate_places(places_required):
@@ -59,116 +58,126 @@ def validate_places(places_required):
 
 
 def enough_places(competition, places_required):
-    return int(competition['numberOfPlaces']) >= places_required
+    return int(competition["numberOfPlaces"]) >= places_required
 
 
 def enough_points(club, places_required):
-    return int(club['points']) >= places_required
+    return int(club["points"]) >= places_required
 
 
 def book_places(club, competition, places_required):
-    competition['numberOfPlaces'] = str(
-        int(competition['numberOfPlaces']) - places_required)
+    """Update clubs and competitions JSON and current loaded data"""
+
+    competition["numberOfPlaces"] = str(
+        int(competition["numberOfPlaces"]) - places_required
+    )
     update_competitions()
-    club['points'] = str(int(club['points']) - places_required)
-    club_data = find_club_by_name(club['name'])
+    club["points"] = str(int(club["points"]) - places_required)
+    club_data = find_club_by_name(club["name"])
     if club_data:
-        club_data['points'] = club['points']
+        club_data["points"] = club["points"]
         update_clubs()
 
 
 app = Flask(__name__)
-app.secret_key = 'something_special'
+app.secret_key = "something_special"
 
 competitions = load_competitions()
 clubs = load_clubs()
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.template_filter('is_past')
+@app.template_filter("is_past")
 def is_past(date):
-    converted_date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+    converted_date = datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
     return converted_date < datetime.today()
 
 
-@app.route('/showSummary', methods=['GET', 'POST'])
+@app.route("/showSummary", methods=["GET", "POST"])
 def show_summary():
-    if request.method == 'POST':
-        club = find_club_by_email(request.form['email'])
+    """Display of the loaded competitions"""
+
+    # Get club from request if this is the first connection
+    if request.method == "POST":
+        club = find_club_by_email(request.form["email"])
     else:
-        club = session.get('club')
+        club = session.get("club")
     if club is None:
         flash("This email is not registered")
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
     else:
-        session['club'] = club
-        return render_template('welcome.html', competitions=competitions)
+        session["club"] = club
+        return render_template("welcome.html", competitions=competitions)
 
 
-@app.route('/book/<competition>')
+@app.route("/book/<competition>")
 def book(competition):
+    """Allow user to book an upcoming competition"""
+
     found_competition = find_competition_by_name(competition)
     if found_competition:
-        if is_past(found_competition['date']):
-            return redirect(url_for('show_summary'))
+        if is_past(found_competition["date"]):
+            return redirect(url_for("show_summary"))
 
-        return render_template('booking.html', competition=found_competition)
+        return render_template("booking.html", competition=found_competition)
     else:
         flash("Something went wrong-please try again")
-        return redirect(url_for('show_summary'))
+        return redirect(url_for("show_summary"))
 
 
-@app.route('/purchasePlaces', methods=['GET', 'POST'])
+@app.route("/purchasePlaces", methods=["GET", "POST"])
 def purchase_places():
-    if request.method == 'GET':
-        return redirect(url_for('show_summary'))
+    if request.method == "GET":
+        return redirect(url_for("show_summary"))
 
-    competition = find_competition_by_name(request.form['competition'])
-    club = session.get('club')
+    competition = find_competition_by_name(request.form["competition"])
+    club = session.get("club")
 
     if not club:
         flash("This club is not registered")
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
 
-    if find_club_by_name(club['name']) is None:
+    if find_club_by_name(club["name"]) is None:
         flash("This club is not registered")
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
 
     if not competition:
         flash("This competition is not registered")
-        return redirect(url_for('show_summary'))
+        return redirect(url_for("show_summary"))
 
-    left_places = int(competition['numberOfPlaces'])
-    places_required = int(request.form['places'])
-    points = int(club['points'])
+    left_places = int(competition["numberOfPlaces"])
+    places_required = int(request.form["places"])
+    points = int(club["points"])
 
     if not validate_places(places_required):
         flash(f"Place must be between 0 and 12")
-        return render_template('booking.html', competition=competition)
+        return render_template("booking.html", competition=competition)
 
     if not enough_places(competition, places_required):
         flash("There is only {} places available".format(left_places))
-        return render_template('booking.html', competition=competition)
+        return render_template("booking.html", competition=competition)
 
     if not enough_points(club, places_required):
         flash("You have only {} points available".format(points))
-        return render_template('booking.html', competition=competition)
+        return render_template("booking.html", competition=competition)
 
     book_places(club, competition, places_required)
-    flash('Great-booking complete!')
-    return redirect(url_for('show_summary'))
+    flash("Great-booking complete!")
+    return redirect(url_for("show_summary"))
 
 
-@app.route('/points', methods=['GET'])
+@app.route("/points", methods=["GET"])
 def see_points():
-    return render_template('points.html', clubs=clubs)
+    """Display points of all the clubs"""
+
+    return render_template("points.html", clubs=clubs)
 
 
-@app.route('/logout')
+@app.route("/logout")
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
---
+++ tests/functional/test_server.py
@@ -0,0 +1,113 @@
+import os
+import threading
+import time
+from datetime import datetime, timedelta
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from server import app
+
+@pytest.fixture(scope='function', autouse=True)
+def set_data(mocker):
+    tomorrow = datetime.today() + timedelta(days=1)
+    tomorrow = tomorrow.strftime("%Y-%m-%d %H:%M:%S")
+    mock_competitions = [
+        {
+            "name": "Test Competition",
+            "date": tomorrow,
+            "numberOfPlaces": "25"
+        },
+        {
+            "name": "Spring Festival",
+            "date": "2025-04-15 13:00:00",
+            "numberOfPlaces": "20"
+        }
+    ]
+
+    mock_clubs = [
+        {
+            "name": "Test Club",
+            "email": "john@simplylift.co",
+            "points": "10"
+        },
+        {
+            "name": "Iron Temple",
+            "email": "admin@irontemple.com",
+            "points": "15"
+        }
+    ]
+
+    mocker.patch('server.clubs', mock_clubs)
+    mocker.patch('server.competitions', mock_competitions)
+    mocker.patch('server.update_clubs', return_value=None)
+    mocker.patch('server.update_competitions', return_value=None)
+
+@pytest.mark.usefixtures("set_data")
+class TestFunctional:
+    @classmethod
+    def setup_class(cls):
+        app.config['TESTING'] = True
+        # use different port in case the "real" server is launched
+        app.config['LIVESERVER_PORT'] = 8943
+        app.config[
+            'DEBUG'] = False
+
+        # Start server on separated thread
+        cls.server_thread = threading.Thread(
+            target=lambda: app.run(port=app.config['LIVESERVER_PORT']))
+        cls.server_thread.daemon = True
+        cls.server_thread.start()
+
+        time.sleep(1)
+
+        cls.browser = webdriver.Firefox()
+        cls.base_url = f"http://localhost:{app.config['LIVESERVER_PORT']}"
+
+    @classmethod
+    def teardown_class(cls):
+        cls.browser.quit()
+
+    def login(self):
+        self.browser.get(f'{self.base_url}')
+        email = self.browser.find_element(By.ID, 'email')
+        email.send_keys('john@simplylift.co')
+        submit = self.browser.find_element(By.ID, 'submit')
+        submit.click()
+
+    def test_index_page(self):
+        self.browser.get(f'{self.base_url}')
+        assert self.browser.title == 'GUDLFT Registration'
+
+    def test_login_page(self):
+        self.login()
+        assert "Welcome, john@simplylift.co" in self.browser.page_source
+
+    def test_points_page(self):
+        self.browser.get(f'{self.base_url}')
+        points_url = self.browser.find_element(By.LINK_TEXT, 'See clubs points')
+        points_url.click()
+        assert self.browser.title == 'Clubs points'
+
+    def test_book_page(self):
+        self.login()
+        points_url = self.browser.find_element(By.LINK_TEXT, 'Book Places')
+        points_url.click()
+        assert 'Test Competition' in self.browser.page_source
+
+    def test_purchase_places(self):
+        self.login()
+        points_url = self.browser.find_element(By.LINK_TEXT, 'Book Places')
+        points_url.click()
+        input_places = self.browser.find_element(By.ID, 'places')
+        input_places.send_keys('5')
+        submit = self.browser.find_element(By.ID, 'submit-places')
+        submit.click()
+        assert 'Great-booking complete!' in self.browser.page_source
+
+
+    def test_logout_page(self):
+        self.login()
+        self.browser.find_element(By.LINK_TEXT, 'Logout').click()
+        assert 'Welcome to the GUDLFT Registration Portal!' in self.browser.page_source
+
